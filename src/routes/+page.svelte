ec1796d0dd5783d988b1befb529be5527f891126
Mike Cheng <minyuc2@illinois.edu>
2025-03-04 23:54:54 -0600

    set up sveltekit with tailwindcss and static adapter

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../app.css';
+	let count = 0;
+</script>
+
+<h1 class="text-3xl">Counter: {count}</h1>
+<button on:click={() => count++}>Increment</button>
+<button on:click={() => count--}>Decrement</button>
