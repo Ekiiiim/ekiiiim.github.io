dd9ebca81938f825b2fe22c835552c93932cb4ad
Mike Cheng <minyuc2@illinois.edu>
2025-03-05 00:10:40 -0600

    chore: minor change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,3 +6,4 @@
 <h1 class="text-3xl">Counter: {count}</h1>
 <button on:click={() => count++}>Increment</button>
 <button on:click={() => count--}>Decrement</button>
+<button on:click={() => (count = 0)}>Reset</button>
