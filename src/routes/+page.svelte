072b4f50e843be4de21d8d02d786a1fba210c183
Mike Cheng <minyuc2@illinois.edu>
2025-03-05 19:38:42 -0600

    smooth scrolling on clicking nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import { base } from '$app/paths';
 
 	let sections = ['About', 'Projects', 'Work', 'Life'];
-	let currentSection = 'About';
+	let currentSection = '';
 
 	function handleScroll() {
 		const sectionElements = document.querySelectorAll('section');
@@ -20,6 +20,7 @@
 	}
 
 	onMount(() => {
+		handleScroll();
 		window.addEventListener('scroll', handleScroll);
 		return () => window.removeEventListener('scroll', handleScroll);
 	});
@@ -31,34 +32,36 @@
 		<span class="text-2xl font-bold">Mike Cheng</span>
 	</div>
 	{#each sections as section}
-		<a
-			href={`#${section}`}
-			class={`mb-2 block rounded p-2 ${currentSection === section ? 'bg-blue-500 text-white' : 'text-gray-800'}`}
+		<button
+			on:click={() => {
+				document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+			}}
+			class={`rounded p-2 ${currentSection === section ? 'bg-blue-500 text-white' : 'text-gray-800'}`}
 		>
 			{section}
-		</a>
+		</button>
 	{/each}
 </div>
 
 <div class="ml-56 p-5">
-	<section id="About" class="mb-10">
+	<section id="About" class="mb-10 scroll-mt-5">
 		<h1 class="text-2xl font-bold">About</h1>
-		<p class=" h-screen">This is the about section.</p>
+		<p class="h-screen">This is the about section.</p>
 	</section>
 
-	<section id="Projects" class="mb-10">
+	<section id="Projects" class="mb-10 scroll-mt-5">
 		<h1 class="text-2xl font-bold">Projects</h1>
 		<p>Here are some of my projects.</p>
 		<div class="h-screen">this should be in cards format...</div>
 	</section>
 
-	<section id="Work" class="mb-10">
+	<section id="Work" class="mb-10 scroll-mt-5">
 		<h1 class="text-2xl font-bold">Work</h1>
 		<p>Details about my professional work experience.</p>
 		<div class="h-screen">timeline here...</div>
 	</section>
 
-	<section id="Life" class="mb-10 h-1/2">
+	<section id="Life" class="scroll-mt-5">
 		<h1 class="text-2xl font-bold">Life</h1>
 		<blockquote class="border-l-4 border-blue-500 pl-4 italic">I will put a quote here</blockquote>
 		<p class="h-screen">Frisbee, English Corner, Beatboxing...</p>
