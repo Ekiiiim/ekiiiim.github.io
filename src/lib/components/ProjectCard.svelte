63bfd5ed4b07d2fff3515b0164660c8f5087d36d
Mike Cheng <minyuc2@illinois.edu>
2025-03-07 21:03:54 -0600

    feat: project card content

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+	const title = 'Auto Valet Parking';
+	const description =
+		'Path planning for autonomous vehicle parking, using hybrid A* and optimization-based method to calculate trajectory.';
+	const image = '/images/AutoValetParking.png';
+	const url = 'https://github.com/Ekiiiim/AutomatedValetParking';
+
 	const card = $state({
 		flipped: false,
 		flip() {
@@ -8,20 +14,31 @@
 </script>
 
 <button onclick={() => card.flip()} class="h-48 w-72 cursor-pointer perspective-distant">
-	<div
-		class="size-full duration-1000 transform-3d dark:text-white {card.flipped && 'rotate-y-180'}"
-	>
+	<div class="size-full duration-1000 transform-3d {card.flipped && 'rotate-y-180'}">
 		<!-- front -->
 		<div class="absolute inset-0 size-full backface-hidden">
-			<div class="flex size-full flex-col items-center justify-center rounded-xl bg-gray-200/10">
-				Front
+			<div
+				class="flex size-full flex-col items-center justify-center rounded-xl bg-gray-600/50 bg-blend-overlay"
+				style="background-image: url({image}); background-size: cover; background-position: center;"
+			>
+				<div class="text-xl font-bold text-black">{title}</div>
 			</div>
 		</div>
 		<!-- back -->
 		<div class="absolute inset-0 size-full rotate-y-180 backface-hidden">
-			<div class="flex size-full flex-col items-center justify-center rounded-xl bg-gray-600">
-				Back
+			<div class="flex size-full flex-col gap-1 rounded-xl bg-gray-200 p-3 dark:bg-gray-600">
+				<div class="text-start text-xl font-bold">{title}</div>
+				<div class="text-start">{description}</div>
+
+				<a
+					href={url}
+					target="_blank"
+					rel="noopener noreferrer"
+					class="absolute bottom-3 text-start text-blue-500 hover:underline"
+				>
+					GitHub
+				</a>
 			</div>
 		</div>
-	</div>
-</button>
+	</div></button
+>
